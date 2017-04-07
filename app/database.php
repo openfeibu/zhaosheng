ec4f64fc85abeb12aef2322f8be367f8e0df712d
wzj <wzj@04bc8791-72bb-fd4c-bc31-6dab5de8151e>
2017-04-07 02:53:38 +0000

    git-svn-id: https://39.108.144.9:8443/svn/zhaosheng@3 04bc8791-72bb-fd4c-bc31-6dab5de8151e

--- app/database.php
+++ app/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'           => 'mysql',
     // 服务器地址
-    'hostname'       => '127.0.0.1',
+    'hostname'       => '211.66.88.153',
     // 数据库名
     'database'       => 'zhaosheng',
     // 用户名
-    'username'       => 'root',
+    'username'       => 'feibukeji',
     // 密码
-    'password'       => '5PALtJKE1YuijpKS',
+    'password'       => 'bgyrtksithv,1*&($AC',
     // 端口
     'hostport'       => '3306',
     // 连接dsn
